166a3f82eaa848af07cbb33d8a6b007268e70e3f
Anand A J <anandjayakumar2001@gmail.com>
2023-07-09 10:16:39 +0530

    add product details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+// import 'package:shopping_app_using_provider/global_variables.dart';
 import 'package:shopping_app_using_provider/home_page.dart';
+// import 'package:shopping_app_using_provider/product_details_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shopping App',
-      home:HomePage() 
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+          primary: const Color.fromRGBO(254, 206, 1, 1),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontFamily: 'Lato',
+            fontWeight: FontWeight.bold,
+            fontSize: 35,
+          ),
+          titleMedium: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+          bodySmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        useMaterial3: true,
+      ),
+       home: HomePage(),
+      // home: ProductDetailsPage(product: products[0]),
     );
   }
 }
---
+++ lib/product_details_page.dart
@@ -0,0 +1,99 @@
+
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatefulWidget {
+  final Map<String, Object> product;
+  const ProductDetailsPage({super.key,required this.product});
+
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  late int selectedSize;
+  @override
+  void initState() {
+    super.initState();
+    selectedSize = (widget.product['sizes'] as List<int>)[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text('Details',
+                style: Theme.of(context).appBarTheme.titleTextStyle)),
+      ),
+      body: Column(
+        children: [
+          Text(widget.product['title'] as String,
+              style: Theme.of(context).textTheme.titleLarge),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset(widget.product['imageUrl'] as String),
+          ),
+          const Spacer(flex: 2),
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(245, 247, 249, 1),
+                borderRadius: BorderRadius.circular(20)),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Text("₹${widget.product['price'] as double}",
+                      style: Theme.of(context).textTheme.titleLarge),
+                  SizedBox(
+                    height: 50,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: (widget.product['sizes'] as List<int>).length,
+                        itemBuilder: (context, index) {
+                          final size =
+                              (widget.product['sizes'] as List<int>)[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedSize = size;
+                                });
+                              },
+                              child: Chip(
+                                label: Text(
+                                  size.toString(),
+                                ),
+                                backgroundColor: selectedSize == size
+                                    ? Theme.of(context).colorScheme.primary
+                                    : const Color.fromRGBO(245, 247, 249, 1),
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ElevatedButton.icon(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).colorScheme.primary,
+                          minimumSize:const Size(double.infinity, 50)),
+                      label: const Text(
+                        'Add To Cart',
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                      icon:const Icon(Icons.shopping_cart_checkout_outlined,color: Colors.black,),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
